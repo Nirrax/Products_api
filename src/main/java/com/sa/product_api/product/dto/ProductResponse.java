c5905b65aaa63a53f11432dfbb8ff69c79102f64
Nirrax <nirraxx@gmail.com>
2026-04-16 22:22:08 +0200

    feat(product): add dto for product response

---
+++ src/main/java/com/sa/product_api/product/dto/ProductResponse.java
@@ -0,0 +1,13 @@
+package com.sa.product_api.product.dto;
+
+import com.sa.product_api.producer.dto.ProducerResponse;
+import com.sa.product_api.producer.model.Producer;
+import com.sa.product_api.product.model.Product;
+
+import java.util.Map;
+
+public record ProductResponse(Long id, String name, Map<String, Object> attributes, ProducerResponse producer) {
+    public static ProductResponse from(Product product, Producer producer) {
+        return new ProductResponse(product.getId(), product.getName(), product.getAttributes(), ProducerResponse.from(producer));
+    }
+}
