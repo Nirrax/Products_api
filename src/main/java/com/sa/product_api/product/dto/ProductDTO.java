a3ea2d8439da5a36669fe33bd64879eef344d0a4
Nirrax <nirraxx@gmail.com>
2026-04-16 23:02:44 +0200

    refactor(error): change annotations for validation to be approperiate for field type

--- src/main/java/com/sa/product_api/product/dto/ProductDTO.java
+++ src/main/java/com/sa/product_api/product/dto/ProductDTO.java
@@ -1,10 +1,12 @@
 package com.sa.product_api.product.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Map;
 
 public record ProductDTO(@NotBlank(message = "Product name is required") String name,
-                         @NotBlank(message = "Producer ID is required") Long producerId,
-                         @NotBlank(message = "Attributes are required") Map<String, Object> attributes) {
+                         @NotNull(message = "Producer ID is required") Long producerId,
+                         @NotEmpty(message = "Attributes are required") Map<String, Object> attributes) {
 }
