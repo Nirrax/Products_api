de957a79c38a17ecb874a2b76bf398a3baeeb5b1
Nirrax <nirraxx@gmail.com>
2026-04-16 20:57:41 +0200

    feat(producer): add implementation of ProducerService

---
+++ src/main/java/com/sa/product_api/producer/service/ProducerServiceImpl.java
@@ -0,0 +1,36 @@
+package com.sa.product_api.producer.service;
+
+import com.sa.product_api.producer.dto.ProducerDTO;
+import com.sa.product_api.producer.exception.ProducerNotFoundException;
+import com.sa.product_api.producer.model.Producer;
+import com.sa.product_api.producer.repository.ProducerRepository;
+
+import java.util.List;
+
+public class ProducerServiceImpl implements ProducerService {
+    private final ProducerRepository producerRepository;
+
+    public ProducerServiceImpl(ProducerRepository producerRepository) {
+        this.producerRepository = producerRepository;
+    }
+
+    @Override
+    public List<Producer> getProducers() {
+        return this.producerRepository.findAll();
+    }
+
+    @Override
+    public Producer getProducerById(Long id) {
+        return this.producerRepository.findById(id).orElseThrow(() -> new ProducerNotFoundException("Producer not found"));
+    }
+
+    @Override
+    public void deleteProducerById(Long id) {
+        this.producerRepository.findById(id).ifPresent(this.producerRepository::delete);
+    }
+
+    @Override
+    public Producer createProducer(ProducerDTO producerDTO) {
+        return this.producerRepository.save(new Producer(producerDTO.name()));
+    }
+}
