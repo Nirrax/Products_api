775411691b1559819d78c68ae99a39fce172a123
Nirrax <nirraxx@gmail.com>
2026-04-16 22:43:55 +0200

    refactor(product): add repository annotation to ProductRepository

--- src/main/java/com/sa/product_api/product/repository/ProductRepository.java
+++ src/main/java/com/sa/product_api/product/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.sa.product_api.product.repository;
 
 import com.sa.product_api.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
