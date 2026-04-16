7dee44551de4b6c3e1b64967e6bf5bc4f770b231
Nirrax <nirraxx@gmail.com>
2026-04-16 23:21:11 +0200

    refactor(product): add method for fetching filtered products

--- src/main/java/com/sa/product_api/product/repository/ProductRepository.java
+++ src/main/java/com/sa/product_api/product/repository/ProductRepository.java
@@ -2,8 +2,14 @@ package com.sa.product_api.product.repository;
 
 import com.sa.product_api.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(value = "SELECT * FROM PRODUCTS WHERE attributes @> CAST(:filter AS jsonb)", nativeQuery = true)
+    List<Product> findByAttributes(@Param("filter") String filter);
 }
