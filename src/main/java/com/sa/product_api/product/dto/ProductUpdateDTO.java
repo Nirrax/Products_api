35d8313b535d617c60f4742e77dd8d9ee477bc22
Nirrax <nirraxx@gmail.com>
2026-04-16 22:43:28 +0200

    feat(product): add annotation to not skip null fields in json

--- src/main/java/com/sa/product_api/product/dto/ProductUpdateDTO.java
+++ src/main/java/com/sa/product_api/product/dto/ProductUpdateDTO.java
@@ -1,6 +1,9 @@
 package com.sa.product_api.product.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Map;
 
+@JsonInclude(JsonInclude.Include.ALWAYS)
 public record ProductUpdateDTO(String name, Map<String, Object> attributes) {
 }
