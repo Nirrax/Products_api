c77364f86c083e7c1763757cac82bd72eaa62b92
Nirrax <nirraxx@gmail.com>
2026-04-16 20:57:23 +0200

    feat(producer): add interface for ProducerService

---
+++ src/main/java/com/sa/product_api/producer/service/ProducerService.java
@@ -0,0 +1,15 @@
+package com.sa.product_api.producer.service;
+
+import com.sa.product_api.producer.dto.ProducerDTO;
+import com.sa.product_api.producer.model.Producer;
+import com.sa.product_api.producer.repository.ProducerRepository;
+
+import java.util.List;
+
+public interface ProducerService {
+    List<Producer> getProducers();
+    Producer getProducerById(Long id);
+    void deleteProducerById(Long id);
+    Producer createProducer(ProducerDTO producerDTO);
+
+}
