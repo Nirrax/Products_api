35268314b9d99580d044d8be7fa4ef68f21b2043
Nirrax <nirraxx@gmail.com>
2026-04-16 20:23:08 +0200

    feat(producer): add Producer entity

---
+++ src/main/java/com/sa/product_api/producer/model/Producer.java
@@ -0,0 +1,34 @@
+package com.sa.product_api.producer.model;
+
+import com.sa.product_api.product.model.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "PRODUCERS")
+public class Producer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "producer")
+    private Set<Product> products;
+}
