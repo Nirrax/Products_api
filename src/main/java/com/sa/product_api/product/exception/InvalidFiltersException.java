4cc90d229586f6d9d5399d528fbf7eafc2c3fa29
Nirrax <nirraxx@gmail.com>
2026-04-16 23:20:14 +0200

    refactor(error): add error for invalid filters

---
+++ src/main/java/com/sa/product_api/product/exception/InvalidFiltersException.java
@@ -0,0 +1,7 @@
+package com.sa.product_api.product.exception;
+
+public class InvalidFiltersException extends RuntimeException {
+    public InvalidFiltersException(String message) {
+        super(message);
+    }
+}
